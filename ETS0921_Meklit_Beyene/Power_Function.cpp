6320999ef70d74ca54bef37e3251291ac74825d4
Meklit-del <meklitbeyene2023@gmail.com>
2024-12-01 15:42:26 +0300

    Update Power_Function.cpp

--- ETS0921_Meklit_Beyene/Power_Function.cpp
+++ ETS0921_Meklit_Beyene/Power_Function.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+int x,y,power;
+cout<<"please enter the value of x:"<<endl;
+cin>>x;
+if(cin.fail()){
+    cout<<"invalid input"<<endl;
+    return 0;
+    }
+cout<<"please enter the value of y:"<<endl;
+cin>>y;
+if(cin.fail()){
+    cout<<"invalid input"<<endl;
+    return 0;
+}
+power=pow(x,y);
+cout<<"the value of x the power of y is"<<" "<<power<<endl;
+return 0;
 
+}
