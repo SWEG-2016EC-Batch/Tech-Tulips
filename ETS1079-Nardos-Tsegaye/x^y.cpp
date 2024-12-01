104ef1d2437428386ee712b9423ce49216bd08ef
Nardos Tsegaye Seme <tsenardi0@gmail.com>
2024-12-01 19:20:04 +0300

    Update x^y.cpp

--- ETS1079-Nardos-Tsegaye/x^y.cpp
+++ ETS1079-Nardos-Tsegaye/x^y.cpp
@@ -7,8 +7,16 @@ int main()
    int x,y,answer;
    cout<<"Enter the value of X : \n";
    cin>>x;
-    cout<<"Enter the value of Y : \n";
+   if(cin.fail()){
+        cout << "ERROR! invalid input.";
+        return 0;
+    }
+   cout<<"Enter the value of Y : \n";
    cin>>y;
+   if(cin.fail()){
+        cout << "ERROR! invalid input.";
+        return 0;
+    }
    answer=pow(x,y);
    cout<<x<<" the power of "<<y<<" is: "<<answer<<endl;
    return 0;
