15f6a0ca288e4d43858b312616131670e1363dca
Meklit-del <meklitbeyene2023@gmail.com>
2024-12-01 15:44:41 +0300

    Update Salary.cpp

--- ETS0921_Meklit_Beyene/Salary.cpp
+++ ETS0921_Meklit_Beyene/Salary.cpp
@@ -1,1 +1,47 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+int main(){
+string employeeName;
+double weeklyHour,bonusRate,baseSalary;
+double bonusPayment,grossSalary,pensionDeduction,taxDeduction,netSalary;
+const double pensionRate=0.05;
+const double taxRate=0.15;
 
+cout<<"please enter employee's name:"<<endl;
+cin>>employeeName;
+if(cin.fail()){
+    cout<<"invalid input."<<endl;
+return 0;
+}
+cout<<"please enter weekly working hours:"<<endl;
+cin>>weeklyHour;
+if(cin.fail()){
+    cout<<"invalid input. please enter a positive number!"<<endl;
+return 0;
+}
+cout<<"please enter bonus rate per hour:"<<endl;
+cin>>bonusRate;
+if(cin.fail()){
+    cout<<"invalid input. please enter a positive number!"<<endl;
+return 0;
+}
+cout<<"please enter base salary:"<<endl;
+cin>>baseSalary;
+if(cin.fail()){
+    cout<<"invalid input. please enter a positive number!"<<endl;
+return 0;
+}
+ bonusPayment=weeklyHour*bonusRate;
+ grossSalary=baseSalary+bonusPayment;
+ pensionDeduction=grossSalary*pensionRate;
+ taxDeduction=(grossSalary-pensionDeduction)*taxRate;
+ netSalary=grossSalary-pensionDeduction-taxDeduction;
+
+cout<<"employee's name is:"<<" "<<employeeName<<endl;
+cout<<"bonus payment is:"<<" "<<bonusPayment<<endl;
+cout<<"gross salary is:"<<" "<<grossSalary<<endl;
+cout<<"net salary is:"<<" "<<netSalary<<endl;
+return 0;
+}
