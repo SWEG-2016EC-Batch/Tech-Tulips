960af191934e7e6cc800f165cda9280cf50f7afb
Nardos Tsegaye Seme <tsenardi0@gmail.com>
2024-12-01 21:22:33 +0300

    Create BMI Calculator.cpp

---
+++ ETS1079-Nardos-Tsegaye/BMI Calculator/BMI Calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int count;
+ double w,h, BMI;
+do{
+ cout<<"Input your weight "<<endl;
+ cin>>w;
+ cout<<"Input your height "<<endl;
+ cin>>h;
+ BMI=w/(h*h);
+ cout<<"Your Body mass Index is : "<<BMI<<endl;
+ if(BMI<18.5){
+     cout<<"Under weight. \n";
+ }
+ else if(BMI>=18.5 && BMI<=24.9){
+    cout<<"Normal weight. \n";
+ }else {
+     cout<<"Over weight."<<endl;
+	 }
+ cout<<"Enter 0 to stop or 1 to continue"<<endl;
+ cin>>count;
+ }while(count!=0);
+    return 0;
+}
