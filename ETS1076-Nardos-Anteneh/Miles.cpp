a92f66b885ee06e4dcda2e623e77fcfe0991d985
Nardos Anteneh Tilahun <audree489@gmail.com>
2024-12-02 21:36:33 +0300

    Update Miles.cpp

--- ETS1076-Nardos-Anteneh/Miles.cpp
+++ ETS1076-Nardos-Anteneh/Miles.cpp
@@ -1,22 +1,19 @@
-/* This program prompts the fuel tank capacity in gallons of an automobile and the miles per gallons the automobiles can be driven.
-*  Then it outputs the number miles the automobile can be driven without refueling.
-*/
 #include <iostream>
 using namespace std;
 
 int main() {
-    // Declare the variables
-    double miles ,tank_capacity, miles_per_Gallon, miles_driven;
+    double tankCapacity, milesPerGallon, totalMiles;
 
-    // Input tank capacity in gallons and miles per gallon
-    cout << "Enter the capacity of the fuel tank of an automobile in gallons: ";
-    cin >> tank_capacity;
-    cout << "Enter the miles per gallon of the automobile can be driven: ";
-    cin >> miles_per_Gallon;
+    cout << "Enter the fuel tank capacity (in gallons): ";
+    cin >> tankCapacity;
 
-    // Calculate the  miles and print it
-    miles = tank_capacity * miles_per_Gallon;
-    cout << "The automobile can be driven " << miles << " miles without refueling." << endl;
+    cout << "Enter the miles per gallon (mpg) the car can travel: ";
+    cin >> milesPerGallon;
+
+    // Calculate total miles that can be driven without refueling
+    totalMiles = tankCapacity * milesPerGallon;
+
+    cout << "The car can travel " << totalMiles << " miles without refueling." << endl;
 
     return 0;
 }
