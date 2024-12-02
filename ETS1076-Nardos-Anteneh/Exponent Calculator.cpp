72e96dd33588c42030896654e9a800c089e3ae34
Nardos Anteneh Tilahun <audree489@gmail.com>
2024-12-02 21:58:27 +0300

    Update Exponent Calculator.cpp

--- ETS1076-Nardos-Anteneh/Exponent Calculator.cpp
+++ ETS1076-Nardos-Anteneh/Exponent Calculator.cpp
@@ -1,25 +1,22 @@
-// This program is used to determine  the expression like X^Y in computers.
-
 #include <iostream>
-#include <cmath>  // For the pow function
+#include <cmath>  // For pow() to do the power calculation
+
 using namespace std;
 
 int main() {
-    // Declare variables
     double x, y, result;
 
-    // Input values from the user
-    cout << "Enter the value of x: ";
+    // Ask the user for the base and exponent values
+    cout << "Hey! Enter the base (x): ";
     cin >> x;
-
-    cout << "Enter the value of y: ";
+    cout << "Now, enter the exponent (y): ";
     cin >> y;
 
-    // Calculate x^y using the pow function from the cmath library
+    // Calculate x raised to the power of y
     result = pow(x, y);
 
-    // Display the result
-    cout << "Result of " << x << "^" << y << " is: " << result << endl;
+    // Show the result
+    cout << x << " raised to the power of " << y << " is: " << result << endl;
 
     return 0;
 }
