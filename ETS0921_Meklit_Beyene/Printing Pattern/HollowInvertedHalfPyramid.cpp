39170d82c0d19e57082a5d431eea0856fd06bed4
Meklit-del <meklitbeyene2023@gmail.com>
2024-12-13 20:39:09 +0300

    Update HollowInvertedHalfPyramid.cpp

--- ETS0921_Meklit_Beyene/Printing Pattern/HollowInvertedHalfPyramid.cpp
+++ ETS0921_Meklit_Beyene/Printing Pattern/HollowInvertedHalfPyramid.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+            for (int i=6; i >= 1; i--) {
+                for (int j=i; j>0; j--){
+                    if(i==1i==6j==1||j==i){
+                    cout << "*";
+                    }
+                    else{
+                    cout<<" ";
+                    }
+                }cout << endl;
+            }
+    return 0;
+}
