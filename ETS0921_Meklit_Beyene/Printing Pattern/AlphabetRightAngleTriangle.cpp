86a930b6e54ab58e35a4fd4f0a2211fb85544830
Meklit-del <meklitbeyene2023@gmail.com>
2024-12-13 20:15:27 +0300

    Update AlphabetRightAngleTriangle.cpp

--- ETS0921_Meklit_Beyene/Printing Pattern/AlphabetRightAngleTriangle.cpp
+++ ETS0921_Meklit_Beyene/Printing Pattern/AlphabetRightAngleTriangle.cpp
@@ -1,1 +1,14 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char alphabet='A';
+   for(int i=0; i<=4; i++){
+        for(char alphabet='A'; alphabet<='A'+i; alphabet++){
+            cout<<alphabet;
+        }cout<<endl;
+    }
+}
