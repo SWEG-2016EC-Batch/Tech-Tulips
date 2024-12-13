a7d2d5d8d39ccf60aa0f860a6116387353f7a56b
Nardos Assefa Gonfa <nardiassefa97@gmail.com>
2024-12-13 22:16:58 +0300

    Create full_pyramid.cpp

---
+++ ETS1077-Nardos-Assefa/printing patterns/user input/full_pyramid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int r; //r=row
+cout<<"Enter row: ";
+cin>>r; 
+     for (int i=1; i <= r; i++) {
+                for (int j = 1; j <=r - i; j++){
+                    cout << " ";
+                }
+                for (int j = 1; j <= i; j++){
+                    cout << "* ";
+                }cout << endl;
+            }
+            cout <<endl;
+}
