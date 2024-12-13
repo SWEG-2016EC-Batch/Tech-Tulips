ecfcc7b1e1c894e5a5759481322e1991e03c80be
Meklit-del <meklitbeyene2023@gmail.com>
2024-12-13 20:25:22 +0300

    Update SmallLetterRectangle.cpp

--- ETS0921_Meklit_Beyene/Printing Pattern/SmallLetterRectangle.cpp
+++ ETS0921_Meklit_Beyene/Printing Pattern/SmallLetterRectangle.cpp
@@ -1,1 +1,13 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+       for(int i=1; i<=5; i++){
+        for(char alphabet='a'; alphabet<='e'; alphabet++){
+            cout<<alphabet;
+        }cout<<endl;
+    }
+}
