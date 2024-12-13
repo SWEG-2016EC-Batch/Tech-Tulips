52d9237f0e35602a9c31d93064b67449481929c3
Nardos Meressa Gulelat <nardosmeressa5@gmail.com>
2024-12-13 10:15:39 -0800

    Create alphabet-small letter-rectangle.cpp

---
+++ ETS 1078_Nardos_Meressa/printing pattern/user input/alphabet-small letter-rectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int r,c; //r=row
+cout<<"Enter row: ";
+cin>>r;
+
+       for(int i=1; i<=r; i++){
+        for(char alphabet='a'; alphabet<='e'; alphabet++){
+            cout<<alphabet;
+        }cout<<endl;
+    }
+}
